c905dd24a07fc2fe56ea79b32cd7d13934569174
Госпожа <@DESKTOP-I5OD79H>
2023-05-20 11:13:15 +0300

    Добавьте файлы проекта.

---
+++ 1.1 лаб/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("A = ");
+double A = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("B = ");
+double B = Convert.ToDouble(Console.ReadLine());
+
+double C = A + B;
+Console.WriteLine("A + B = " + C);
+
+C = A - B;
+Console.WriteLine("A - B = " + C);
+
+
+//C = A * B;
+//Console.WriteLine("A * B = " + C);
+
+//C = A / B;
+//Console.WriteLine("A / B = " + C);
